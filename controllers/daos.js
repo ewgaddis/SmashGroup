a1a2629385a1add46c2a4977286ed0653f612349
Dantley P Rogers <ckbzo@live.com>
2015-04-01 19:59:35 -0600

    dao
    findUser

--- controllers/daos.js
+++ controllers/daos.js
@@ -1,33 +1,27 @@
 var util = require('../util');
+var crypto = require('crypto');
+var mongoose = require(('mongoose'));
 
-// Add an object/document to a collection
-function addObject(collection, object, callback) {
-	collection.insert(object, callback);
-}
+User = mongoose.model('User');
 
-// Adds a user Javascript object to the users collection of the given database
-// callback = function(err, result)
-function addUser(database, user, callback) {
-	database.collection("users", function(err, coll) { // Assuming the users collection is called "users"
-		if(!err) {
-			addObject(userCollection, function(err, result) {
-				callback(err, result);
-			});
-		} else {
-			callback(err, null);
+
+exports.findUser(userName, callback)
+{
+	User.findOne({username:userName}).exec(function(err,user)
+	{
+
+		if(!user)
+		{
+			user=null;
 		}
+		callback(user,err);
 	});
 }
 
-function findUser(username, callback) {
-	// Find user in database.
+exports.adduser(user)
+{
 	
-	callback(null, {
-		username: "Tester",
-		password: util.hashPW("Test")
-	});
 }
 
-// Make it so that other JS files can use the function
-exports.addUser  = addUser;
-exports.findUser = findUser;
+//adduser para (nam)
+//findgroup
