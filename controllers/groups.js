139b2581516c83604299ccf65615f980e933a717
ijdavies <ian.davies309@gmail.com>
2015-04-14 18:49:38 -0600

    implementing newGroup view

--- controllers/groups.js
+++ controllers/groups.js
@@ -139,5 +139,10 @@ exports.addMember = function(req, res, next) {
 };
 
 exports.createNewGroup = function(req,res,next){
-	
+
+    console.log(JSON.stringify(req.body));
+	//var admins = [req.session.user];
+	//var cats = req.body.
+	//daos.addGroup(req.body.newGroupName, description, '', zipcode, admins, admins, [], )
+
 }
