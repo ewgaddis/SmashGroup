899a3cb7ce01f957b6d2379a17e2fbffa0df178f
SmashGroup <ec2-user@ip-172-31-30-65.us-west-2.compute.internal>
2015-04-19 22:50:45 +0000

    ec2 push

--- util.js
+++ util.js
@@ -1,6 +1,6 @@
 var crypto = require('crypto');
 
-var HOST_NAME = 'localhost'; // NOTE: Change this to proper host when done testing.
+var HOST_NAME = '52.11.21.224'; // NOTE: Change this to proper host when done testing.
 
 exports.getHostName = function() {
 	return HOST_NAME;
